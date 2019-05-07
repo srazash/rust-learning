209264c24f3978902914688c65fa6f6a80aba341
Ryan Shaw-Harrison <srazash@protonmail.com>
2019-05-07 21:59:38 +0100

    Added validation
    Tweaked code, added secret_number validation.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use rand::Rng;
 fn main() {
 
     let mut secret_number = String::new();
-    let mut guess_count: i32 = 0;
+    let mut guess_count: u32 = 0;
 
     println!("Welcome to the reverse guessing game!");
     println!("Please provide a secret number between 1 - 100:");
 
     io::stdin().read_line(&mut secret_number).expect("Error!");
 
-    println!("The secret number has been set to: {}", secret_number);
+    println!("The secret number has been set to: {}", secret_number.trim());
 
     loop {
         let guess: u32 = rand::thread_rng().gen_range(1, 101);
@@ -24,15 +24,20 @@ fn main() {
             Err(_) => continue,
         };
 
-        match secret_number.cmp(&guess) {
+        if secret_number < 1 || secret_number > 100 {
+            println!("The secret number must be between 1 - 100! Please try again!");
+            break;
+        } else {
+            match secret_number.cmp(&guess) {
             Ordering::Less => { println!("GUESS #{}: The computer guessed {}, which was too low!", guess_count, guess); guess_count += 1; },
             Ordering::Greater => { println!("GUESS #{}: The computer guessed {}, which was too high!", guess_count, guess); guess_count += 1; },
             Ordering::Equal => {
                 println!("The computer guessed {} which is CORRECT!", guess);
                 println!("The computer got the correct number after {} guesses!", guess_count);
                 break;
-            },
+                },
+            }
         }
-
+        
     }
 }
