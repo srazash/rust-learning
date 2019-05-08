c4c4b064cd23b880cd69097d3031be7c92b9b6a4
Ryan Shaw-Harrison <srazash@protonmail.com>
2019-05-08 15:25:58 +0100

    Fixed validation
    Fixed validation of secret number provided by user.

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-
     let mut secret_number = String::new();
     let mut guess_count: u32 = 0;
-    
+
     println!("Welcome to the reverse guessing game!");
     println!("Please provide a secret number between 1 - 100:");
-
+    
     loop {
-        io::stdin().read_line(&mut secret_number).expect("Error!");
+        let mut sn = secret_number.to_string();
 
-        let secret_number_validation: u32 = match secret_number.trim().parse() {
+        io::stdin().read_line(&mut sn).expect("Error!");
+
+        let secret_number_validation: u32 = match sn.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        // :(
+        if secret_number_validation >= 1 && secret_number_validation <= 100 {
+            secret_number = sn.to_string();
+            break;
+        } else {
+            println!("Please provide a secret number between 1 - 100:");
+        }
+
     }
 
     println!("The secret number has been set to: {}", secret_number.trim());
@@ -34,8 +41,8 @@ fn main() {
         };
 
         match secret_number.cmp(&guess) {
-            Ordering::Less => { println!("GUESS #{}: The computer guessed {}, which was too low!", guess_count, guess); guess_count += 1; },
-            Ordering::Greater => { println!("GUESS #{}: The computer guessed {}, which was too high!", guess_count, guess); guess_count += 1; },
+            Ordering::Less => { println!("GUESS #{}: The computer guessed {}, which was too high!", guess_count, guess); guess_count += 1; },
+            Ordering::Greater => { println!("GUESS #{}: The computer guessed {}, which was too low!", guess_count, guess); guess_count += 1; },
             Ordering::Equal => {
                 println!("The computer guessed {} which is CORRECT!", guess);
                 println!("The computer got the correct number after {} GUESSES!", guess_count);
