1d829f308b110c40ef2d4a0679b932fa34f9b9f1
Ryan Shaw-Harrison <srazash@protonmail.com>
2019-05-30 14:04:01 +0100

    Code update.

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,8 @@ fn main() {
     // loop to guess what the secret number is, will loop until the randomly guessed value matches the user inputted secret number
     // THERE IS A MAJOR ISSUE WITH THIS AS IT CURRENTLY CAN GUESS INCORRECT VALUES MULTIPLE TIMES!
     loop {
-        let mut guess: u32 = 0;
+        let mut guess: u32 = rand::thread_rng().gen_range(1, 101);
+        rand_guess.push(guess);
         let secret_number: u32 = match secret_number.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -47,16 +48,15 @@ fn main() {
         // we want to compare the current guess against previous guesses
         // if the guess hasn't occured before allow the loop to continue
         // otherwise we need to regen the guess
-        loop {
-            guess = rand::thread_rng().gen_range(1, 101);
+        for i in rand_guess.iter() {
+            let mut _guess: u32 = rand::thread_rng().gen_range(1, 101);
 
-            for i in rand_guess.iter() {
-                if i == &guess {
-                    continue;
-                } else {
-                    rand_guess.push(guess);
-                    break;
-                }
+            if i == &_guess {
+                continue;
+            } else {
+                rand_guess.push(_guess);
+                guess = _guess;
+                break;
             }
         }
 
