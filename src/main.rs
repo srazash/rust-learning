ef153437cddf9b63d53625aff9ba647ce2e4eb75
Ryan Shaw-Harrison <srazash@protonmail.com>
2019-06-23 17:27:39 +0100

    Progress commit.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+// chap4 ownership
+// scope
+
 fn main() {
     
     let s = "hello";
+    // variable 's' refers to a string literal
+    // valid from point of declaration forward
+
     
-}
+} // no loner valid as this is the end of the scope of 's'
